3a95f61be05b543856b6a02be9fdb2a96b07b222
Kennashka DeSilva <kennashka@yahoo.com>
2018-11-17 23:01:32 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "my-test-instance" {
   ami             = "${data.aws_ami.ubuntu.id}"
   instance_type   = "t2.micro"
-
+  security_groups=["launch-wizard-14"]
   tags {
     Name = "test-instance"
   }
